ff328d2567804ada708b97005b1794014a4f1ef0
compsci <compsci@compsci>
2017-11-01 09:51:13 -0600

    First commit

---
+++ src/Caesar/CaesarCipher.java
@@ -0,0 +1,81 @@
+package Caesar;
+
+public class CaesarCipher {
+
+	private String alpha = "abcdefghijklmnopqrstuvwxyz0123456789.:'";
+	
+	public String encode(String plainText, int key) {
+		
+		String secretText = "";
+		int index, keyAdded;
+
+		for (int i = 0; i < plainText.length(); i++) {
+			if (plainText.charAt(i) != ' ') {
+				index = alpha.indexOf(plainText.charAt(i));
+				keyAdded = (index + key) % alpha.length();
+				secretText = secretText + alpha.charAt(keyAdded);
+			} else {
+				secretText = secretText + " ";
+			}
+
+		}
+
+
+		return secretText;
+
+	}
+	
+	public String decode(String plainText, int key) {
+		
+		String secretText = "";			// Still need to work on have to reverse
+		int index, keyAdded;
+
+		for (int i = 0; i < plainText.length(); i++) {
+			if (plainText.charAt(i) != ' ') {
+				index = alpha.indexOf(plainText.charAt(i));
+				keyAdded = (index + key) % alpha.length();
+				secretText = secretText + alpha.charAt(keyAdded);
+			} else {
+				secretText = secretText + " ";
+			}
+
+		}
+
+
+		return secretText;
+	}
+	
+	public String cracker(String secretText, int key) {
+		
+	String plainText = "";				// in progress make keys display PlainText in console
+		int keys = 0;
+
+		for (int i = 0; i < secretText.length(); i++) {
+			
+			keys = keys + 1;
+			int index, keyAdded;
+			
+			
+			
+			
+			
+			if (secretText.charAt(i) != ' ') {
+				index = alpha.indexOf(secretText.charAt(i));
+				keyAdded = (index + key) % alpha.length();
+				plainText = plainText + alpha.charAt(keyAdded);
+			} else {
+				plainText = plainText + " ";
+			}
+			
+			
+			
+			
+			
+		}
+
+
+		return plainText;
+	}
+	
+
+}
